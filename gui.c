57d28fc7c18ccd9ff98d149bc643c098d2a7d391
grrlic <18105757 grrlic@users.noreply.github.com>
2020-11-24 02:48:18 +0000

    clang formatted with Google style

--- gui.c
+++ gui.c
@@ -1,123 +1,123 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "gui.h"
 
-GtkWidget * button_higher;
-GtkWidget * frame_box;
-GtkWidget * north_container;
-GtkWidget * panel_container;
-GtkWidget * button_container;
-GtkWidget * info_container;
-GtkWidget * south_container;
-GtkWidget * button_higher;
-GtkWidget * button_lower;
-GtkWidget * button_pass; // Extension part
-GtkWidget * button_new;
-GtkWidget * button_quit;
-GtkWidget * button_hint; // Extension part
-GtkWidget * text_prompt;
-GtkTextBuffer * buffer_prompt;
+GtkWidget* button_higher;
+GtkWidget* frame_box;
+GtkWidget* north_container;
+GtkWidget* panel_container;
+GtkWidget* button_container;
+GtkWidget* info_container;
+GtkWidget* south_container;
+GtkWidget* button_higher;
+GtkWidget* button_lower;
+GtkWidget* button_pass;	 // Extension part
+GtkWidget* button_new;
+GtkWidget* button_quit;
+GtkWidget* button_hint;	 // Extension part
+GtkWidget* text_prompt;
+GtkTextBuffer* buffer_prompt;
 
-void clear_child(GtkWidget * container) {
-    GList * children, * iter;
-    children = gtk_container_get_children(GTK_CONTAINER(container));
-    for (iter = children; iter != NULL; iter = g_list_next(iter))
-        gtk_widget_destroy(GTK_WIDGET(iter -> data));
-    g_list_free(children);
+void clear_child(GtkWidget* container) {
+	GList *children, *iter;
+	children = gtk_container_get_children(GTK_CONTAINER(container));
+	for (iter = children; iter != NULL; iter = g_list_next(iter))
+		gtk_widget_destroy(GTK_WIDGET(iter->data));
+	g_list_free(children);
 }
 
 void clear_container(int container_id) {
-    if (container_id == 0) {
-        clear_child(north_container);
-    } else {
-        clear_child(south_container);
-    }
+	if (container_id == 0) {
+		clear_child(north_container);
+	} else {
+		clear_child(south_container);
+	}
 }
 
-void set_prompt(char * prompt) {
-    gtk_text_buffer_set_text(buffer_prompt, prompt, -1);
+void set_prompt(char* prompt) {
+	gtk_text_buffer_set_text(buffer_prompt, prompt, -1);
 }
 
 void show_ingame_buttons() {
-    gtk_widget_show(button_higher);
-    gtk_widget_show(button_lower);
-    gtk_widget_show(button_pass); // Extension part
-    gtk_widget_show(button_hint); // Extension part
+	gtk_widget_show(button_higher);
+	gtk_widget_show(button_lower);
+	gtk_widget_show(button_pass);  // Extension part
+	gtk_widget_show(button_hint);  // Extension part
 }
 
 void hide_ingame_buttons() {
-    gtk_widget_hide(button_higher);
-    gtk_widget_hide(button_lower);
-    gtk_widget_hide(button_pass); // Extension part
-    gtk_widget_hide(button_hint); // Extension part
+	gtk_widget_hide(button_higher);
+	gtk_widget_hide(button_lower);
+	gtk_widget_hide(button_pass);  // Extension part
+	gtk_widget_hide(button_hint);  // Extension part
 }
 
-void swap(int * a, int * b) {
-    int tmp = * a;
-    * a = * b;
-    * b = tmp;
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
-char * get_image_path(int card) {
-    /************CODE HERE**************/
-    char * path = malloc(20);
+char* get_image_path(int card) {
+	char* path = malloc(20);
 
-    int bound = ((card / 4) * 4 + 4) % 52; // (0-3) -> 4, ..., (15-18) -> 16, ...,(48-51) -> 0
-    int pos = card % 4; // position of the "cards" array
+	int bound = ((card / 4) * 4 + 4) %
+		    52;	     // (0-3) -> 4, ..., (15-18) -> 16, ...,(48-51) -> 0
+	int pos = card % 4;  // position of the "cards" array
 
-    int cards[4] = {0};
+	int cards[4] = {0};
 
-    int i;
-    for (i = 0; i < 4; i++) {
-        cards[i] = bound;
-        bound++;
-    }
+	int i;
+	for (i = 0; i < 4; i++) {
+		cards[i] = bound;
+		bound++;
+	}
 
 	// e.g. card = 15, bound = 16, pos = 3
 	// original cards = [4, 5, 6, 7]
 	// swapped cards = [7, 6, 4, 5]
 
-    // original -> h, s, d, c
-    // convert to
-    // desired -> c, d, h, s
-    swap( & cards[3], & cards[0]); // swapped to c, s, d, h
-    swap( & cards[3], & cards[1]); // swapped to c, h, d, s
-    swap( & cards[2], & cards[1]); // swapped to c, d, h, s
+	// original -> h, s, d, c
+	// convert to
+	// desired -> c, d, h, s
+	swap(&cards[3], &cards[0]);  // swapped to c, s, d, h
+	swap(&cards[3], &cards[1]);  // swapped to c, h, d, s
+	swap(&cards[2], &cards[1]);  // swapped to c, d, h, s
 
-    sprintf(path, "picture/%d.png", cards[pos]);
-    return path;
-    /************CODE END***************/
+	sprintf(path, "picture/%d.png", cards[pos]);
+	return path;
 }
 
 void add_image(int container_id, int card) {
-    GtkWidget * image;
-    GtkWidget * container;
-    if (container_id == 0) {
-        container = north_container;
-    } else {
-        container = south_container;
-    }
-    char * file_name = get_image_path(card);
-    image = gtk_image_new_from_file(file_name);
-    gtk_container_add(GTK_CONTAINER(container), image);
-    gtk_widget_show_all(container);
+	GtkWidget* image;
+	GtkWidget* container;
+	if (container_id == 0) {
+		container = north_container;
+	} else {
+		container = south_container;
+	}
+	char* file_name = get_image_path(card);
+	image = gtk_image_new_from_file(file_name);
+	gtk_container_add(GTK_CONTAINER(container), image);
+	gtk_widget_show_all(container);
 }
 
-void quit_game(GtkWindow * window) {
-    /************CODE HERE**************/
-    // close the game
+void quit_game(GtkWindow* window) {
+	// close the game
 	// This contains extended functionalities
-    GtkDialogFlags flags = GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT; // modal dialog to force user attention
-    GtkWidget * dialog = gtk_message_dialog_new(window,
-												flags,
-												GTK_MESSAGE_QUESTION,
-												GTK_BUTTONS_YES_NO,
-												"Are you sure to quit?");
+	GtkDialogFlags flags =
+	    GTK_DIALOG_MODAL |
+	    GTK_DIALOG_DESTROY_WITH_PARENT;  // modal dialog to force user
+					     // attention
+	GtkWidget* dialog =
+	    gtk_message_dialog_new(window, flags, GTK_MESSAGE_QUESTION,
+				   GTK_BUTTONS_YES_NO, "Are you sure to quit?");
 
-    gtk_window_set_title(GTK_WINDOW(dialog), "Confirmation");
+	gtk_window_set_title(GTK_WINDOW(dialog), "Confirmation");
 
-    int result = gtk_dialog_run(GTK_DIALOG(dialog)); // get response signal
-    switch (result) {
+	int result = gtk_dialog_run(GTK_DIALOG(dialog));  // get response signal
+	switch (result) {
 		case GTK_RESPONSE_YES:
 			gtk_widget_destroy(dialog);
 			gtk_widget_destroy(GTK_WIDGET(window));
@@ -125,59 +125,75 @@ void quit_game(GtkWindow * window) {
 		default:
 			gtk_widget_destroy(dialog);
 			break;
-    }
-    /************CODE END***************/
+	}
 }
 
-void activate(GtkApplication * app, gpointer user_data) {
-    GtkWidget * window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "HighLow");
-    gtk_window_set_default_size(GTK_WINDOW(window), 1200, 400);
-    int padding = 2;
+void activate(GtkApplication* app, gpointer user_data) {
+	GtkWidget* window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "HighLow");
+	gtk_window_set_default_size(GTK_WINDOW(window), 1200, 400);
+	int padding = 2;
 
-    frame_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    north_container = gtk_box_new(FALSE, 0);
-    panel_container = gtk_box_new(FALSE, 0);
-    button_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    info_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    south_container = gtk_box_new(FALSE, 0);
-    gtk_container_set_border_width(GTK_CONTAINER(north_container), 20);
-    gtk_container_set_border_width(GTK_CONTAINER(panel_container), 20);
-    gtk_container_set_border_width(GTK_CONTAINER(button_container), 20);
-    gtk_container_set_border_width(GTK_CONTAINER(info_container), 20);
-    gtk_container_set_border_width(GTK_CONTAINER(south_container), 20);
-    button_higher = gtk_button_new_with_label("Higher!");
-    button_lower = gtk_button_new_with_label("Lower!");
-    button_pass = gtk_button_new_with_label("Pass"); // Extension part
-    button_new = gtk_button_new_with_label("New Game");
-    button_quit = gtk_button_new_with_label("Quit Game");
-    button_hint = gtk_button_new_with_label ("Hint"); // Extension part
-    text_prompt = gtk_text_view_new();
-    buffer_prompt = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_prompt));
+	frame_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	north_container = gtk_box_new(FALSE, 0);
+	panel_container = gtk_box_new(FALSE, 0);
+	button_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	info_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	south_container = gtk_box_new(FALSE, 0);
+	gtk_container_set_border_width(GTK_CONTAINER(north_container), 20);
+	gtk_container_set_border_width(GTK_CONTAINER(panel_container), 20);
+	gtk_container_set_border_width(GTK_CONTAINER(button_container), 20);
+	gtk_container_set_border_width(GTK_CONTAINER(info_container), 20);
+	gtk_container_set_border_width(GTK_CONTAINER(south_container), 20);
+	button_higher = gtk_button_new_with_label("Higher!");
+	button_lower = gtk_button_new_with_label("Lower!");
+	button_pass = gtk_button_new_with_label("Pass");  // Extension part
+	button_new = gtk_button_new_with_label("New Game");
+	button_quit = gtk_button_new_with_label("Quit Game");
+	button_hint = gtk_button_new_with_label("Hint");  // Extension part
+	text_prompt = gtk_text_view_new();
+	buffer_prompt = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_prompt));
 
-    gtk_container_add(GTK_CONTAINER(window), frame_box);
-    gtk_box_pack_start(GTK_BOX(frame_box), north_container, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(frame_box), panel_container, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(panel_container), button_container, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(panel_container), info_container, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(frame_box), south_container, FALSE, FALSE, padding);
+	gtk_container_add(GTK_CONTAINER(window), frame_box);
+	gtk_box_pack_start(GTK_BOX(frame_box), north_container, FALSE, FALSE,
+			   padding);
+	gtk_box_pack_start(GTK_BOX(frame_box), panel_container, FALSE, FALSE,
+			   padding);
+	gtk_box_pack_start(GTK_BOX(panel_container), button_container, FALSE,
+			   FALSE, padding);
+	gtk_box_pack_start(GTK_BOX(panel_container), info_container, FALSE,
+			   FALSE, padding);
+	gtk_box_pack_start(GTK_BOX(frame_box), south_container, FALSE, FALSE,
+			   padding);
 
-    gtk_box_pack_start(GTK_BOX(button_container), button_higher, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(button_container), button_lower, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(button_container), button_pass, FALSE, FALSE, padding); // Extension part
-    gtk_box_pack_start(GTK_BOX(button_container), button_new, FALSE, FALSE, padding);
-    gtk_box_pack_start(GTK_BOX(button_container), button_quit, FALSE, FALSE, padding);
-    gtk_box_pack_start (GTK_BOX (button_container), button_hint, FALSE, FALSE, padding); // Extension part
-    g_signal_connect_swapped(button_quit, "clicked", G_CALLBACK(quit_game), window);
-    gtk_box_pack_start(GTK_BOX(info_container), text_prompt, FALSE, FALSE, padding);
+	gtk_box_pack_start(GTK_BOX(button_container), button_higher, FALSE,
+			   FALSE, padding);
+	gtk_box_pack_start(GTK_BOX(button_container), button_lower, FALSE,
+			   FALSE, padding);
+	gtk_box_pack_start(GTK_BOX(button_container), button_pass, FALSE, FALSE,
+			   padding);  // Extension part
+	gtk_box_pack_start(GTK_BOX(button_container), button_new, FALSE, FALSE,
+			   padding);
+	gtk_box_pack_start(GTK_BOX(button_container), button_quit, FALSE, FALSE,
+			   padding);
+	gtk_box_pack_start(GTK_BOX(button_container), button_hint, FALSE, FALSE,
+			   padding);  // Extension part
+	g_signal_connect_swapped(button_quit, "clicked", G_CALLBACK(quit_game),
+				 window);
+	gtk_box_pack_start(GTK_BOX(info_container), text_prompt, FALSE, FALSE,
+			   padding);
 
-    g_signal_connect(button_new, "clicked", G_CALLBACK(new_game), NULL);
-    g_signal_connect(button_higher, "clicked", G_CALLBACK(on_click_higher), NULL);
-    g_signal_connect(button_lower, "clicked", G_CALLBACK(on_click_lower), NULL);
-    g_signal_connect(button_pass, "clicked", G_CALLBACK(on_click_pass), NULL); // Extension part
-    g_signal_connect(button_hint, "clicked", G_CALLBACK(on_click_hint), (gpointer) window); // Extension part
-    gtk_text_buffer_set_text(buffer_prompt, "", -1);
+	g_signal_connect(button_new, "clicked", G_CALLBACK(new_game), NULL);
+	g_signal_connect(button_higher, "clicked", G_CALLBACK(on_click_higher),
+			 NULL);
+	g_signal_connect(button_lower, "clicked", G_CALLBACK(on_click_lower),
+			 NULL);
+	g_signal_connect(button_pass, "clicked", G_CALLBACK(on_click_pass),
+			 NULL);	 // Extension part
+	g_signal_connect(button_hint, "clicked", G_CALLBACK(on_click_hint),
+			 (gpointer)window);  // Extension part
+	gtk_text_buffer_set_text(buffer_prompt, "", -1);
 
-    gtk_widget_show_all(window);
-    hide_ingame_buttons();
+	gtk_widget_show_all(window);
+	hide_ingame_buttons();
 }
--- highlow.c
+++ highlow.c
@@ -1,178 +1,186 @@
-#include <time.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "gui.h"
 
 int add_new_card(int container_id, int step);
-char * get_prompt();
+char *get_prompt();
 void new_game();
 void card_shuffle();
 void end_game();
 void higher_lower(int is_higher);
 void on_click_higher();
 void on_click_lower();
-void on_click_pass(); // Extension part
-void on_click_hint(); // Extension part
+void on_click_pass();  // Extension part
+void on_click_hint();  // Extension part
 
-static int status[3]; // step, North's score, South's score
+static int status[3];  // step, North's score, South's score
 static int card_deck[52];
 const int rounds = 19;
 char prompt[100];
 
 // container_id = 0 for North, 1 for South
 int add_new_card(int container_id, int step) {
-    int card = card_deck[step];
-    add_image(container_id, card);
-    return card;
+	int card = card_deck[step];
+	add_image(container_id, card);
+	return card;
 }
 
-char * get_prompt() {
-    char north[] = "North";
-    char south[] = "South";
-    sprintf(prompt, "%s's turn\nNorth's score : %d\n South's score: %d", (status[0] % 2 == 0 ? north : south), status[1], status[2]);
-    return prompt;
+char *get_prompt() {
+	char north[] = "North";
+	char south[] = "South";
+	sprintf(prompt, "%s's turn\nNorth's score : %d\n South's score: %d",
+		(status[0] % 2 == 0 ? north : south), status[1], status[2]);
+	return prompt;
 }
 
 void new_game() {
-    /************CODE HERE**************/
-    // starts a new game,
-    // all initialization useful for resetting should be done here
-    // Start from scratch or
-    // finished one game and start another one.
+	// starts a new game,
+	// all initialization useful for resetting should be done here
+	// Start from scratch or
+	// finished one game and start another one.
 
-    int i;
-    for (i = 0; i < 3; i++) {
-        status[i] = 0; // reset status to zero
-    }
-    clear_container(0); // reset North
-    clear_container(1); // reset South
+	int i;
+	for (i = 0; i < 3; i++) {
+		status[i] = 0;	// reset status to zero
+	}
+	clear_container(0);  // reset North
+	clear_container(1);  // reset South
 
-    char * new_prompt = get_prompt();
-    set_prompt(new_prompt); // clear existing and set a blank new prompt
+	char *new_prompt = get_prompt();
+	set_prompt(new_prompt);	 // clear existing and set a blank new prompt
 
-    card_shuffle(); // shuffle the card deck
+	card_shuffle();	 // shuffle the card deck
 
-    // 2. initialize two cards to be pre-shown at the start of the game
-    add_new_card(0, 0); // Add card to North player
-    add_new_card(1, 1); // Add card to South player
-    status[0] = 2; // After two initial cards given, increment initial steps by two
+	// 2. initialize two cards to be pre-shown at the start of the game
+	add_new_card(0, 0);  // Add card to North player
+	add_new_card(1, 1);  // Add card to South player
+	status[0] =
+	    2;	// After two initial cards given, increment initial steps by two
 
-    show_ingame_buttons(); // show higher, lower, pass, etc. buttons
-    /************CODE END***************/
+	show_ingame_buttons();	// show higher, lower, pass, etc. buttons
 }
 
 void card_shuffle() {
-    // this method prepares all cards in card_deck
-    // this is the ONLY function that should apply RANDOMNESS
-    int i, irandom, temp;
-    for (i = 0; i < 52; i++) {
-        card_deck[i] = i;
-    }
-    for (i = 0; i < 51; i++) {
-        irandom = i + rand() % (52 - i);
-        temp = card_deck[i];
-        card_deck[i] = card_deck[irandom];
-        card_deck[irandom] = temp;
-    }
+	// this method prepares all cards in card_deck
+	// this is the ONLY function that should apply RANDOMNESS
+	int i, irandom, temp;
+	for (i = 0; i < 52; i++) {
+		card_deck[i] = i;
+	}
+	for (i = 0; i < 51; i++) {
+		irandom = i + rand() % (52 - i);
+		temp = card_deck[i];
+		card_deck[i] = card_deck[irandom];
+		card_deck[irandom] = temp;
+	}
 }
 
 void end_game() {
-    /************CODE HERE**************/
-    // 1. Hide the higher and lower button
-    hide_ingame_buttons();
+	// 1. Hide the higher and lower button
+	hide_ingame_buttons();
 
-    // 2. show match results in buffer_prompt. If North wins, Please show "North wins.\nNorth's score : %d, South's score: %d"
-    char * results = malloc(60); // allocate memory to store winner message
-    int score = status[1] - status[2]; // positive if North has higher score or tie with South
+	// 2. show match results in buffer_prompt. If North wins, Please show
+	// "North wins.\nNorth's score : %d, South's score: %d"
+	char *results = malloc(60);  // allocate memory to store winner message
+	int score =
+	    status[1] -
+	    status[2];	// positive if North has higher score or tie with South
 
-    if (score == 0) {
-        sprintf(results, "Draw.\nNorth's score : %d, South's score: %d", status[1], status[2]);
-    } else if (score > 0) {
-        sprintf(results, "North wins.\nNorth's score : %d, South's score: %d", status[1], status[2]);
-    } else {
-        sprintf(results, "South wins.\nNorth's score : %d, South's score: %d", status[1], status[2]);
-    }
+	if (score == 0) {
+		sprintf(results, "Draw.\nNorth's score : %d, South's score: %d",
+			status[1], status[2]);
+	} else if (score > 0) {
+		sprintf(results,
+			"North wins.\nNorth's score : %d, South's score: %d",
+			status[1], status[2]);
+	} else {
+		sprintf(results,
+			"South wins.\nNorth's score : %d, South's score: %d",
+			status[1], status[2]);
+	}
 
-    set_prompt(results); // update prompt with message in `results` array
-    /************CODE END***************/
+	set_prompt(results);  // update prompt with message in `results` array
 }
 
 void higher_lower(int is_higher) {
-    int step = status[0];
-    int current_container = step % 2;
-    int card = add_new_card(current_container, step);
+	int step = status[0];
+	int current_container = step % 2;
+	int card = add_new_card(current_container, step);
 
-    /************CODE HERE**************/
-    // If current player predict correct card, he get 10 points. Otherwise, he loss 5 points.
-    int last_card = card_deck[step - 1];
-    int card_diff = card - last_card; // negative only if current card is lower than last card
-    int player = step % 2; // 0 for North, 1 for South
+	// If current player predict correct card, he get 10 points. Otherwise,
+	// he loss 5 points.
+	int last_card = card_deck[step - 1];
+	int card_diff =
+	    card -
+	    last_card;	// negative only if current card is lower than last card
+	int player = step % 2;	// 0 for North, 1 for South
 
-    switch (is_higher) {
-    case 0: // when player press "Lower" button
-        if (card_diff < 0) status[player + 1] += 10; // player +1 -> North = 1, South = 2
-        else status[player + 1] -= 5;
-        break;
-    case 1: // when player press "Higher" button
-        if (card_diff > 0) status[player + 1] += 10;
-        else status[player + 1] -= 5;
-        break;
-        // anything besides 0 or 1, e.g. "Pass" button will have no effect on scoreboard
-    }
-    /************CODE END***************/
+	switch (is_higher) {
+		case 0:	 // when player press "Lower" button
+			if (card_diff < 0)
+				status[player + 1] +=
+				    10;	 // player +1 -> North = 1, South = 2
+			else
+				status[player + 1] -= 5;
+			break;
+		case 1:	 // when player press "Higher" button
+			if (card_diff > 0)
+				status[player + 1] += 10;
+			else
+				status[player + 1] -= 5;
+			break;
+			// anything besides 0 or 1, e.g. "Pass" button will have
+			// no effect on scoreboard
+	}
 
-    if (status[0] == rounds) {
-        end_game();
-        return;
-    } else {
-        status[0]++;
-        char * prompt = get_prompt();
-        set_prompt(prompt);
-    }
+	if (status[0] == rounds) {
+		end_game();
+		return;
+	} else {
+		status[0]++;
+		char *prompt = get_prompt();
+		set_prompt(prompt);
+	}
 }
 
-void on_click_higher() {
-    higher_lower(1);
-}
+void on_click_higher() { higher_lower(1); }
 
-void on_click_lower() {
-    higher_lower(0);
-}
+void on_click_lower() { higher_lower(0); }
 
-void on_click_pass() {
-    higher_lower(-1);
-}
+void on_click_pass() { higher_lower(-1); }
 
 void on_click_hint(GtkWidget *widget, gpointer window) {
-	GtkDialogFlags flags = GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT; // modal dialog to force user attention
+	GtkDialogFlags flags =
+	    GTK_DIALOG_MODAL |
+	    GTK_DIALOG_DESTROY_WITH_PARENT;  // modal dialog to force user
+					     // attention
 	GtkWidget *dialog;
-	char * buffer = malloc(200);
+	char *buffer = malloc(200);
 
 	int step = status[0];
-	int last_card = card_deck[step -1];
+	int last_card = card_deck[step - 1];
 	int remaining = 52 - step;
 	int higher = 0;
 	int lower = 0;
-	
+
 	int i;
 	// compare all remaining cards with the last card
-	for (i = step + 1; i < 52; i ++){
+	for (i = step + 1; i < 52; i++) {
 		if (card_deck[i] > last_card) higher++;
 		if (card_deck[i] < last_card) lower++;
 	}
 
-	sprintf(buffer, "Remaining cards: %d\n"
-					"No. of cards higher than opponent\'s last card: %d\n"
-					"No. of cards lower than opponent\'s last card: %d",
-					remaining,
-					higher,
-					lower);
+	sprintf(buffer,
+		"Remaining cards: %d\n"
+		"No. of cards higher than opponent\'s last card: %d\n"
+		"No. of cards lower than opponent\'s last card: %d",
+		remaining, higher, lower);
 
-	dialog = gtk_message_dialog_new(GTK_WINDOW(window),
-									flags,
-									GTK_MESSAGE_INFO,
-									GTK_BUTTONS_OK,
-									buffer);
+	dialog =
+	    gtk_message_dialog_new(GTK_WINDOW(window), flags, GTK_MESSAGE_INFO,
+				   GTK_BUTTONS_OK, buffer);
 	gtk_window_set_title(GTK_WINDOW(dialog), "Information");
 	gtk_dialog_run(GTK_DIALOG(dialog));
 	gtk_widget_destroy(dialog);
