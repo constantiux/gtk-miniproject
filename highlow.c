864119fe241ae12aac71eb21ce6603c4d313bb17
grrlic <18105757 grrlic@users.noreply.github.com>
2020-11-29 14:45:20 +0000

    fix typo

--- highlow.c
+++ highlow.c
@@ -183,23 +183,23 @@ void on_click_hint(GtkWidget *widget, gpointer window) {
 }
 
 void on_click_cheat() {
-	GtkWidget *window, *image, *label;
+	GtkWidget *cheat, *image, *label;
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-	gtk_window_set_default_size(GTK_WINDOW(window), 280, 150);
-	gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
-	gtk_window_set_modal(GTK_WINDOW(window), TRUE);
-	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-	gtk_container_set_border_width(GTK_CONTAINER(window), 0);
+	cheat = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position(GTK_WINDOW(cheat), GTK_WIN_POS_CENTER);
+	gtk_window_set_default_size(GTK_WINDOW(cheat), 280, 150);
+	gtk_window_set_decorated(GTK_WINDOW(cheat), FALSE);
+	gtk_window_set_modal(GTK_WINDOW(cheat), TRUE);
+	gtk_window_set_resizable(GTK_WINDOW(cheat), FALSE);
+	gtk_container_set_border_width(GTK_CONTAINER(cheat), 0);
 
 	int next_card = card_deck[status[0]]; // the current step is "to be revealed" card
 	char *imagepath = get_image_path(next_card); // use function declared in gui.h
-	image = gtk_image_new_from_file(file_name);
+	image = gtk_image_new_from_file(imagepath);
 	gtk_container_add(GTK_CONTAINER(window), image);
 
 	char *description = malloc(50);
-	sprintf(description, "Your next card will be:\n")
+	sprintf(description, "Your next card will be:\n");
 	label = gtk_label_new(description);
 	gtk_container_add(GTK_CONTAINER(window), label);
 
